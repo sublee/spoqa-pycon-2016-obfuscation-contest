64f9ab3266057dc941512ab627cc721a982e9560
Heungsub Lee <sub@subl.ee>
2016-08-15 01:51:33 +0900

    I love Spoqa

--- calc.py
+++ calc.py
@@ -1,2 +1,14 @@
-import sys
-print(eval(sys.argv[1]))
+x=__builtins__.__dict__.__getitem__;xx=xxxx=bytes;y=yy=yyy=xx.decode;(
+
+      x(bytes   ([ 0o160      ,114      ,105, 11  *10,116
+    ]).decode   ())(x(yyy    (xxxx([   0x65,0x76  ,97,98+10
+    ])          ))(    x(   yy     ((  xx    (([         95
+    ,95,21*5+   1-1   ,109 ,28     *4  ,9    **2   +0000+30
+          ,114 ,116   ,95  ,95     ])  ))    ))(  xx     ([
+    115,122-1  ,115*1*1])   .decode(   )).__dict__[y(xxxx([
+    97,114,     3+ 10**2      ,118      ]))][ ((   1<<0) )]
+                ))                            ==
+                10                            >>
+                42                            <0
+
+)                # by Heungsub Lee <sub@subl.ee>
